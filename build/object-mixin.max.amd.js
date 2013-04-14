c31d030d06c004d4b71e6915ccba24224e8f771e
Andrea Giammarchi <agiammarchi@twitter.com>
2013-04-14 01:34:59 -0700

    attempt number one

--- build/object-mixin.max.amd.js
+++ build/object-mixin.max.amd.js
@@ -20,4 +20,115 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 */
-define({});
+define(function () {/*jslint browser: true, forin: true, plusplus: true, indent: 4 */
+(function(Object, mixin) {
+    "use strict"; // happy linter ^_____^
+
+    /* <droppable>
+     * adhoc polyfill section for this purpose only
+     * never use these functions outside this closure ... like ...
+ne*/var
+    /*
+^ ... you see that? only reason I chose 4 spaces indentations here :D
+      also this comment ... pure quality, right ?!?! ... anyway ... */
+
+        // for IE < 9 Desktop browsers
+        defineProperty = Object.defineProperty ||
+        function (o, k, d) {
+            o[k] = d.value;
+        },
+        // same as above
+        getOwnPropertyNames = Object.getOwnPropertyNames ||
+        function (o) {
+            var
+                // in case the guy does not inherit from Object.prototype
+                has = Object.prototype.hasOwnProperty,
+                result = [],
+                key;
+            for (key in o) {
+                // in non ES5 compliant browsers
+                // there's no way to define properties
+                // as non enumerable unless these are
+                // there by default, like "constructor" is
+                // for functions.prototype
+                if (has.call(o, key)) {
+                    result.push(key);
+                }
+            }
+            return result;
+        },
+        // again ... IE < 8
+        getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor ||
+        function (o, k) {
+            return {
+                enumerable: true,
+                writable: true,
+                configurable: true,
+                value: o[k]
+            };
+        };
+    // </droppable>
+
+    // if already defined get out of here
+    // this should be 
+    // if (mixin in Object) return;
+    // but for some reason I went for JSLint ... 
+    if (Object[mixin]) {
+        return;
+    }
+    // same descriptor as other spec'd methods
+    defineProperty(
+        Object,
+        mixin,
+        {
+            enumerable: false,
+            writable: true,
+            configurable: true,
+            value: function mixin(
+                target, // object to enrich with
+                source    // mixin object/function
+            ) {
+                var
+                    // check if source is a function
+                    enricher = typeof source === 'function' ? source.prototype : source,
+                    // per each own property name
+                    keys = getOwnPropertyNames(enricher),
+                    length = keys.length,
+                    i = 0,
+                    key;
+                while (i < length) {
+                    // define it ...
+                    defineProperty(
+                        target,
+                        key = keys[i++],
+                        // ... via same property descriptor
+                        getOwnPropertyDescriptor(
+                            source,
+                            key
+                        )
+                    );
+                }
+                // if the object had no own names
+                // it's quite clear the intention of the user
+                // so that if a function without properties
+                // is passed through this method ...
+                if (!length && typeof source === 'function') {
+                    // this function is invoked with the target
+                    // as its own context
+                    source.apply(
+                        target,
+                        // optional arguments to initialize defaults
+                        // for this mixin might be accepted too
+                        keys.slice.call(arguments, 2)
+                    );
+                }
+                // always return the initial target
+                // ignoring a possible different return
+                // in latter case: consistency with this method
+                return target;
+            }
+        }
+    );
+}(Object, 'mixin'));
+return Object.mixin;
+});
--- test/object-mixin.js
+++ test/object-mixin.js
@@ -1,13 +1,76 @@
 //remove:
-var main = require('../build/object-mixin.node.js');
+var mixin = require('../build/object-mixin.node.js');
 //:remove
 
 wru.test([
+  // ES3 compatible tests
   {
     name: "main",
     test: function () {
-      wru.assert(typeof main == "object");
-      // wru.assert(0);
+      wru.assert(typeof Object.mixin == "function");
+    }
+  },{
+    name: "classic object",
+    test: function () {
+      var obj = Object.mixin({}, {a:'a'});
+      wru.assert('has property', 'a' in obj);
+      wru.assert('with the right value', obj.a === 'a');
+    }
+  },{
+    name: "function",
+    test: function () {
+      function method() {}
+
+      function mixin(){
+        this.method = method;
+      }
+      delete mixin.prototype.constructor;
+
+      var obj = Object.mixin({}, mixin);
+      wru.assert('invoked', obj.method === method);
+    }
+  },{
+    name: "function with arguments",
+    test: function () {
+      function method() {}
+
+      function mixin(a, b){
+        this.a = a;
+        this.b = b;
+        this.method = method;
+      }
+      delete mixin.prototype.constructor;
+
+      var obj = Object.mixin({}, mixin, 1, 2);
+      wru.assert('invokedwith args',
+        obj.method === method &&
+        obj.a === 1 &&
+        obj.b === 2
+      );
+    }
+  },
+  // ES5+ compatible tests
+  {
+    name: "descriptors",
+    test: function () {
+      if (Object.getOwnPropertyDescriptor) {
+        function get() {
+          wru.assert(true);
+          obj = 123;
+        }
+        var obj = Object.mixin(
+          {},
+          Object.defineProperty(
+            {},
+            'key',
+            {
+              get: wru.async(get)
+            }
+          )
+        );
+        obj.key;
+        wru.assert(obj === 123);
+      }
     }
   }
 ]);
