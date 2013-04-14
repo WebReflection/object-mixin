8dcbe84d8a436439f7dee9c4240995b4e6779e67
Andrea Giammarchi <agiammarchi@twitter.com>
2013-04-14 12:27:38 -0700

    simplified

--- build/object-mixin.node.js
+++ build/object-mixin.node.js
@@ -1,71 +1,124 @@
 /*!
-Copyright (C) 2013 by WebReflection
+           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                   Version 2, December 2004
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
+ 0. You just DO WHAT THE FUCK YOU WANT TO.
 */
 /*jslint browser: true, forin: true, plusplus: true, indent: 4 */
 (function(Object, mixin) {
     "use strict"; // happy linter ^_____^
 
-    /* <droppable>
-     * adhoc polyfill section for this purpose only
+    /* <droppable> interesting code after line 110, here
+     * ad-hoc polyfill section for this purpose only
      * never use these functions outside this closure ... like ...
 ne*/var
-    /*
-^ ... you see that? only reason I chose 4 spaces indentations here :D
-      also this comment ... pure quality, right ?!?! ... anyway ... */
 
-        // for IE < 9 Desktop browsers
+        // borrowed methods for unknown Objects
+        ObjectPrototype = Object.prototype,
+
+        lookupGetter = ObjectPrototype.__lookupGetter__,
+        lookupSetter = ObjectPrototype.__lookupSetter__,
+        defineGetter = ObjectPrototype.__defineGetter__,
+        defineSetter = ObjectPrototype.__defineSetter__,
+        has          = ObjectPrototype.hasOwnProperty,
+
+        slice        = Array.prototype.slice,
+
+        // for IE < 9 and non IE5 yet browsers
         defineProperty = Object.defineProperty ||
         function (o, k, d) {
-            o[k] = d.value;
-        },
-        // same as above
-        getOwnPropertyNames = Object.getOwnPropertyNames ||
-        function (o) {
             var
-                // in case the guy does not inherit from Object.prototype
-                has = Object.prototype.hasOwnProperty,
-                result = [],
-                key;
-            for (key in o) {
-                // in non ES5 compliant browsers
-                // there's no way to define properties
-                // as non enumerable unless these are
-                // there by default, like "constructor" is
-                // for functions.prototype
-                if (has.call(o, key)) {
-                    result.push(key);
-                }
+                get = d.get, // has.call(d, 'get') would be better but
+                set = d.set; // ES5 is just like this
+            if (get && defineGetter) {
+                defineGetter.call(o, k, get);
             }
-            return result;
+            if (set && defineSetter) {
+                defineSetter.call(o, k, set);
+            }
+            if (!(get || set)) {
+                o[k] = d.value;
+            }
         },
-        // again ... IE < 8
+        // for IE < 9 and non IE5 yet browsers
+        getOwnPropertyNames = Object.getOwnPropertyNames ||
+        (function () {
+            var
+                addHiddenOwnProperties = function (result) {
+                    return result;
+                },
+                list = [],
+                key,
+                i,
+                length;
+
+            for (key in {valueOf: key}) {
+                list.push(key);
+            }
+
+            if (!list.length) {
+                length = list.push(
+                    'constructor',
+                    'hasOwnProperty',
+                    'isPrototypeOf',
+                    'propertyIsEnumerable',
+                    'toLocaleString',
+                    'toString',
+                    'valueOf'
+                ) - 1;
+                addHiddenOwnProperties = function (result, o) {
+                    for (i = 0; i < length; i++) {
+                        key = list[i];
+                        if (has.call(o, key)) {
+                            result.push(key);
+                        }
+                    }
+                    return result;
+                };
+            }
+
+            return function (o) {
+                var
+                    result = [],
+                    key;
+                for (key in o) {
+                    if (has.call(o, key)) {
+                        result.push(key);
+                    }
+                }
+                return addHiddenOwnProperties(result, o);
+            };
+        }()),
+        // IE < 9 or other non ES5 yet browsers
         getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor ||
         function (o, k) {
-            return {
-                enumerable: true,
-                writable: true,
-                configurable: true,
-                value: o[k]
-            };
+            var
+                descriptor = {
+                    enumerable: true,
+                    configurable: true
+                },
+                get = lookupGetter && lookupGetter.call(o, k),
+                set = lookupSetter && lookupSetter.call(o, k);
+            if (get) {
+                descriptor.get = get;
+            }
+            if (set) {
+                descriptor.set = set;
+            }
+            if (!(get || set)) {
+                descriptor.writable = true;
+                descriptor.value = o[k];
+            }
+            return descriptor;
         };
     // </droppable>
 
@@ -86,45 +139,59 @@ ne*/var
             configurable: true,
             value: function mixin(
                 target, // object to enrich with
-                source    // mixin object/function
+                source  // mixin object or function
             ) {
                 var
-                    // check if source is a function
-                    enricher = typeof source === 'function' ? source.prototype : source,
-                    // per each own property name
-                    keys = getOwnPropertyNames(enricher),
-                    length = keys.length,
-                    i = 0,
+                    i,
+                    length,
+                    keys,
                     key;
-                while (i < length) {
-                    // define it ...
-                    defineProperty(
-                        target,
-                        key = keys[i++],
-                        // ... via same property descriptor
-                        getOwnPropertyDescriptor(
-                            enricher,
-                            key
-                        )
-                    );
-                }
-                // if the object had no own names
-                // it's quite clear the intention of the user
-                // so that if a function without properties
-                // is passed through this method ...
-                if (!length && typeof source === 'function') {
-                    // this function is invoked with the target
-                    // as its own context
-                    source.apply(
-                        target,
-                        // optional arguments to initialize defaults
-                        // for this mixin might be accepted too
-                        keys.slice.call(arguments, 2)
-                    );
+
+                if (typeof source === 'function') {
+                    // if the source is a function
+                    // it will be invoked with object as target
+                    // this let us define mixin as closures
+                    // function addFunctionality() {
+                    //     this.functionality = function () {
+                    //       // do amazing stuff
+                    //     }
+                    // }
+                    // addFunctionality.call(Class.prototype);
+                    // addFunctionality.call(genericObject);
+                    // // or
+                    // Object.mixin(Class.prototype, addFunctionality);
+
+                    // try to perform as fast as possible
+                    if (arguments.length < 3) {
+                        // so if no extra args are passed ...
+                        source.call(target);
+                    } else {
+                        // there is no need to slice them as done here
+                        source.apply(target, slice.call(arguments, 2));
+                    }
+                } else {
+                    // if source is an object
+                    // grab all possibe properties
+                    // and per each of them ...
+                    keys = getOwnPropertyNames(source);
+                    length = keys.length;
+                    i = 0;
+                    while (i < length) {
+                        key = keys[i++];
+                        // ... define it ...
+                        defineProperty(
+                            target,
+                            key,
+                            // ... using the same descriptor
+                            getOwnPropertyDescriptor(
+                                source,
+                                key
+                            )
+                        );
+                    }
                 }
                 // always return the initial target
-                // ignoring a possible different return
-                // in latter case: consistency with this method
+                // ignoring all possible different return with functions
                 return target;
             }
         }
